2045ff51b74968f8f99a1df074d98c97befcf984
Maurilio Valenti <maurilio.valenti@gmail.com>
2025-09-16 16:46:25 +0200

    modifiche prova

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,9 +53,9 @@ const Navbar = () => {
             <Link to="/area-utenti">
               <Button
                 variant="default"
-                className="bg-[#FF6B6C] hover:bg-[#e85a5a] transition-smooth"
+                className="bg-[#FF6B6B] hover:bg-[#e85a5a] transition-smooth"
               >
-                Area Utenti
+                CIAO
               </Button>
             </Link>
           </div>
